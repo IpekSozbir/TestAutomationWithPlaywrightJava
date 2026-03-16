d1e3cc083df5e18024ac7d586137dd9cb456812a
IpekSozbir <ipekoren@gmail.com>
2026-03-16 22:46:07 +0300

    v5

---
+++ src/test/java/C06_ElementMethods.java
@@ -0,0 +1,41 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.awt.*;
+
+public class C06_ElementMethods {
+
+    public static void main(String[] args) throws InterruptedException {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+        // .set.SlowMo(); icine yazdigimiz yuzde orani kadar testi yavaslatarak test sirasinda islemleri gorebilmemizi saglar
+        Page page = browser.newPage();
+
+        page.navigate("https://www.testotomasyonu.com");
+        page.fill("//*[@class='search-label']","iphone");
+        // page.fill(); ile bir input box'a metin girisi yapabiliyoruz
+        // page.fill(); icerisinde string halde istedigimiz locate'i ve yine string halde girmek istedigimiz metni veriyoruz
+
+        page.keyboard().press("Enter");
+        //  page.keyboard().press(); belirtilen elemente bir klavye tusu gonderir
+
+        page.hover("//*[@class='lazy']");
+        // page.hover(); belirtilen elementin uzeirne gelir
+
+        Thread.sleep(3000);
+        page.waitForLoadState();
+        System.out.println(page.title());
+
+        page.close();
+        browser.close();
+        playwright.close();
+
+    }
+
+}
